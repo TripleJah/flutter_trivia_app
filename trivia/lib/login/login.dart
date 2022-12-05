d966cd039c143c68ef5b024ae5d1e90130d562bf
TripleJah <orangehedge@protonmail.com>
2022-12-05 17:06:50 -0600

    Login/signout screen

--- trivia/lib/login/login.dart
+++ trivia/lib/login/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:trivia/services/auth..dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({ Key? key }) : super(key: key);
@@ -6,8 +8,57 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      body: Container(
+        padding: const EdgeInsets.all(35),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const FlutterLogo(
+              size: 160,
+            ),
+            Flexible(
+              child: LoginButton(text: 'Continue as Guest', icon: FontAwesomeIcons.userNinja, color: Colors.deepPurple, 
+              loginMethod: AuthService().anonLogin,
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+  final Function loginMethod;
+
+
+  const LoginButton(
+    {super.key,
+    required this.text,
+    required this.icon,
+    required this.color,
+    required this.loginMethod});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: ElevatedButton.icon(
+        icon: Icon(
+          icon,
+          color:Colors.white,
+          size: 20,
+        ),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(28),
+          backgroundColor: color,
+        ), 
+        onPressed: () => loginMethod(),
+        label: Text(text, textAlign: TextAlign.center,),
       ),
     );
   }
--- trivia/lib/services/auth..dart
+++ trivia/lib/services/auth..dart
@@ -4,4 +4,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final userStream = FirebaseAuth.instance.authStateChanges();
   final user = FirebaseAuth.instance.currentUser;
+
+  Future<void> anonLogin() async {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } on FirebaseAuthException { //there is a catch (e) in the guide here
+      // error handled
+    }
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 }
+
